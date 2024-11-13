1fef16322b42f2692493683fac4a1b0a86c0a3b1
LopasReng <galo24bj@student.ju.se>
2024-11-13 11:09:41 +0100

    creating BackGround

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ function setup() {
   createCanvas(800, 600);
 }
 
+  fill (100, 100, 100);
+  noStroke();
+  rect(0, 450, width, 150);
+
+}
+
 function draw() {
-  background(255, 140, 0);
+  BackGround();
 }
